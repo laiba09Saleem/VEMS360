d2c941a69cd9ac1eae8e09af48fdfaecd827d194
Laiba Saleem <laibasaleem068@gmail.com>
2025-10-20 02:33:11 +0500

    feat: add form and click handlers

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('#searchForm').on('submit', function(e) {
+        e.preventDefault();
+    });
+
+    $('.view-venue').on('click', function() {
+    });
+
+    $('#contactForm').on('submit', function(e) {
+        e.preventDefault();
+    });
+});
